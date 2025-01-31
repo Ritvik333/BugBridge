fc36adc289584b5658397ff1f3e60dd30f479dc7
ritvik <rt308602@dal.ca>
2025-01-31 10:42:17 -0400

    #26 Initial setup

---
+++ backend/src/main/java/com/example/demo/Controller/RegistrationController.java
@@ -0,0 +1,27 @@
+package com.example.demo.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Model.User;
+import com.example.demo.Model.UserRepository;
+
+@RestController
+public class RegistrationController {
+    
+    @Autowired
+    private UserRepository myAppUserRepository;
+    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
+    @PostMapping(value = "/req/signup", consumes = "application/json")
+    public User createUser(@RequestBody User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return myAppUserRepository.save(user);
+    }
+    
+}
