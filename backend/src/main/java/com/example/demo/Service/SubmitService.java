6121ec3d4a0d76673779ddab17ed75878d19adb1
ritvik <rt308602@dal.ca>
2025-03-03 11:55:37 -0400

    #43 service implementation to save submissions

--- backend/src/main/java/com/example/demo/Service/SubmitService.java
+++ backend/src/main/java/com/example/demo/Service/SubmitService.java
@@ -1,6 +1,11 @@
 package com.example.demo.Service;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -32,9 +37,33 @@ public class SubmitService {
         Bug bug = bugRepository.findById(bugId)
             .orElseThrow(() -> new RuntimeException("Bug not found"));
         System.out.println(user);
+        Path directoryPath = Paths.get(storagePath, userId + "_" + username, "submissions");
+        String extension = mapLanguageToExtension(bug.getLanguage());
+        String filename = userId + "_" + bugId + extension;
+        Path filePath = directoryPath.resolve(filename);
+    
+        Files.createDirectories(filePath.getParent());
+        Files.write(filePath, code.getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+    
+        // Submit existingSubmission = submitRepository.findByUserIdAndBugId(userId, bugId);
         
+        // if (existingSubmission != null) {
+        //     existingSubmission.setCodeFilePath(filePath.toString());
+        //     return submitRepository.save(existingSubmission);
+        // } else {
+        //     Submit submit = new Submit();
+        //     submit.setUser(user);
+        //     submit.setBug(bug);
+        //     submit.setDescription(desc);
+        //     submit.setCodeFilePath(filePath.toString());
+        //     return submitRepository.save(submit);
+        // }
         Submit submit = new Submit();
-        return submitRepository.save(submit);
+            submit.setUser(user);
+            submit.setBug(bug);
+            submit.setDescription(desc);
+            submit.setCodeFilePath(filePath.toString());
+            return submitRepository.save(submit);
     }
 
     public String mapLanguageToExtension(String language) {
@@ -46,4 +75,5 @@ public class SubmitService {
             default: return ".txt";
         }
     }
+    
 }
