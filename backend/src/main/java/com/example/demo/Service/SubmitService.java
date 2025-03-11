a0a4b7471f85c9954678ba4e4c1e5fde410b12eb
ritvik <rt308602@dal.ca>
2025-03-11 12:34:47 -0300

    #43 solutions implementation

--- backend/src/main/java/com/example/demo/Service/SubmitService.java
+++ backend/src/main/java/com/example/demo/Service/SubmitService.java
@@ -7,7 +7,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
